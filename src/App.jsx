f91c791f09bc5e71932162458f83cb92c883528a
brocodus <brocodus@example.com>
2025-09-07 18:24:47 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./App.css";
 import Skills from "./components/Skills";
 import Home from "./components/Home";
@@ -8,22 +7,6 @@ import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Resume from "./components/Resume";
 import Contact from "./components/Contact";
-
-// function App() {
-//   return (
-//     <div  style={{ margin: 0, padding: 0 }}>
-//       <Navbar />
-//       <section id="home"><Home /></section>
-//       <section id="about"><About /></section>
-//       <section id="experience"><Experience /></section>
-//       <section id="skills"><Skills /></section>
-//       <section id="projects"><Projects /></section>
-//       <section id="resume"><Resume /></section>
-//       <section id="contact"><Contact /></section>
-//     </div>
-//   );
-// }
-
 
 function App() {
   return (
